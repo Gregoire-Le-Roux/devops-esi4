6b15b5f51c671b689313dad524adfcebcbabcbc8
Gregoire-Le-Roux <gregoire.leroux.pro@gmail.com>
2023-04-26 14:26:53 +0200

    test: 💍 add test of todolist delete all button

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -3,21 +3,25 @@ import {beforeEach, describe, expect, test} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from '../src/App';
 
-describe("Accordion test", () => {
+describe("Todolist test", () => {
+    // Before each test, simulate render of App (Todolist)
     beforeEach(() => {
         render(<App></App>);
     });
+
+    // Get input texte to add an item in todolist
     const getInputTexte = () => {
         const inputTexte = screen.getByLabelText('texte-input') as HTMLInputElement
         return inputTexte
     };
     
-    test("should show title all the time", () => {
-        // Titre h1 soit ToDoList
+    test("should show todolist title all the time", () => {
+        // title h1 = ToDoList
         expect(screen.getByText("ToDoList")).toBeDefined()
     })
 
-    test('should add text in input then add it to todolist', async () => { 
+    // Input, Validate and Delete element test
+    test('should add text in input then add it to todolist and finally delete it', async () => { 
         // Add text in input
         const inputTexte = getInputTexte()
         fireEvent.change(inputTexte, { target: { value: 'Lorem' } })
@@ -38,5 +42,27 @@ describe("Accordion test", () => {
 
         screen.debug()
         expect(screen.queryByText(/Lorem/i)).toBeNull()
+    })
+
+    // Supprimer tout button
+    test('should delete all element in todolist', async () => { 
+        const inputTexte = getInputTexte()
+        const buttonValidate = screen.getByText(/Valider/i);
+        fireEvent.change(inputTexte, { target: { value: 'Lorem' } })
+        fireEvent.click(buttonValidate)
+        fireEvent.change(inputTexte, { target: { value: 'Ipsum' } })
+        fireEvent.click(buttonValidate)
+
+        // Verify if 2 elements added in todolist
+        expect(screen.queryByText(/Lorem/i)).toBeDefined();
+        expect(screen.queryByText(/Ipsum/i)).toBeDefined();
+
+        
+        // Delete all elements in todolist
+        const buttonSupprimerTout = screen.getByText("Supprimer tout");
+        fireEvent.click(buttonSupprimerTout)
+
+        expect(screen.queryByText(/Lorem/i)).toBeNull()
+        expect(screen.queryByText(/Ipsum/i)).toBeNull()
     })
 })
